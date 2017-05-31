ed63e020b5f984bfd5c9019376f7f999da423d9b
adri4silva <adri4silva@gmail.com>
2017-05-31 19:21:07 +0200

    Direct message method addeded + Javadoc of the class

--- twitterbot/Function.java
+++ twitterbot/Function.java
@@ -1,40 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package twitterbot;
 
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import twitter4j.DirectMessage;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
 /**
- *
+ * This class is related to all the functionalities of the app.
  * @author adri
  */
 public class Function {
 
     private static final Twitter twitter = TwitterFactory.getSingleton();
 
-    ;
- 
-    public static void tweet() {
+    /**
+     * Publish a tweet with a certain 'latestStatus' message.
+     * @param latestStatus
+     */
+    public static void tweet(String latestStatus) {
+
         try {
-            String latestStatus = "Prueba bot 1";
             Status status = twitter.updateStatus(latestStatus);
 
             System.out.println("Cambiado el estado satisfactoriamente [" + status.getText() + "].");
         } catch (TwitterException ex) {
             System.out.println("Error al actualizar el estado");
         }
+
     }
+    
+    /**
+     * Gets the timeline of the user.
+     */
 
     public static void getTimeline() {
+
         try {
             List<Status> statuses = twitter.getHomeTimeline();
             System.out.println("Timeline: ");
@@ -45,6 +47,25 @@ public class Function {
         } catch (TwitterException ex) {
             System.out.println("Timeline no disponible");
         }
+
+    }
+
+    /**
+     * Sends a direct message 'content' to a user 'recipientId'.
+     * @param recipientId
+     * @param content 
+     */
+    
+    public static void directMessage(String recipientId, String content) {
+
+        try {
+            DirectMessage message = twitter.sendDirectMessage(recipientId, content);
+
+            System.out.println("Enviado: " + message.getText() + " to @" + message.getRecipientScreenName());
+        } catch (TwitterException ex) {
+            System.out.println("No se pudo enviar el mensaje");
+        }
+
     }
 
 }
